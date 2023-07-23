ecc9402da72b03aeadd8121bd39bf95360024788
Jassocc <clnearle@gmail.com>
2023-07-23 11:51:23 -0700

    string function

---
+++ string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_s - takes as string and return
+ * @str:string
+ * Return: string
+ */
+
+char *print_s(va_list str)
+{
+	char *s, *p;
+	int len;
+
+	s = va_arg(str, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
+	len = _strlen(s);
+	p = malloc(sizeof(char) * len + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	return (_strcpy(p, s));
+}
