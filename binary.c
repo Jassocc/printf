120ba421e548395fddbaa1d78f1d5eb130d475be
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-23 20:11:30 +0300

    added the binary.c file

---
+++ binary.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * itob - change int binary
+ * @list: int to change
+ * Return: string with binary
+ */
+char *itob(va_list list)
+{
+	int j = 0, two = 1;
+	int i, k;
+	char *s;
+
+	k = va_arg(list, int);
+	i = k;
+
+	s = malloc(sizeof(char) * 33);
+	if (s == NULL)
+		return (NULL);
+
+	if (k < 0)
+	{
+		s[0] = 1 + '0';
+		j++;
+		k *= -1;
+		i *= -1;
+	}
+
+	while (k < 1)
+	{
+		k /= 2;
+		two *= 2;
+	}
+
+	while (two > 0)
+	{
+		s[j++] = (i / two + '0');
+		i %= two;
+		two /= 2;
+	}
+
+	s[j] = '\0';
+
+	return (s);
+}
