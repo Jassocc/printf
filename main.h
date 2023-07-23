19f804185bb389ed44363f76127f2ee934ef502f
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-23 20:00:40 +0300

    added the main.h file

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef main_h
+#define main_h
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char* (*get_func(char i))(va_list);
+char *create_buffer(void);
+void write_buffer(char *buffer, int len, va_list list);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+int _printf(const char *format, ...);
+char *print_s(va_list list);
+char *print_c(va_list list);
+char *print_d(va_list list);
+char *itob(va_list list);
+char *rot13(va_list list);
+char *rev_string(va_list list);
+char *itoOctal(va_list list);
+
+/**
+ * structypes - struct
+ * @id: identifier of type print
+ * @func: ptr to funcs that according to indentifier
+ */
+typedef struct types
+{
+	char id;
+	char* (*func)(va_list);
+} printf;
+
+#endif
