565a05b8f2f872edac0e42cc8c9df3d7925094a3
Jassocc <clnearle@gmail.com>
2023-07-24 09:57:53 -0700

    fixes

--- print.c
+++ print.c
@@ -25,11 +25,9 @@ int check_buffer_overflow(char *buffer, int len)
  * Return: strings with identifiers
  */
 int _printf(const char *format, ...)
-{
-	int len = 0, total_len = 0, i = 0, j;
+{	int len = 0, total_len = 0, i = 0, j;
 	va_list list;
-	char *buffer, *str;
-	char *(*f)(va_list);
+	char *buffer, *str, *(*f)(va_list);
 
 	if (format == NULL)
 		return (-1);
@@ -40,44 +38,31 @@ int _printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] != '%')
-		{	len = check_buffer_overflow(buffer, len);
-			buffer[len++] = format[i++];
-			total_len++; }
+		{len = check_buffer_overflow(buffer, len), buffer[len++] = format[i++];
+		total_len++; }
 		else
 		{	i++;
 			if (format[i] == '\0')
-			{	va_end(list);
-				free(buffer);
+			{va_end(list), free(buffer);
 				return (-1); }
 			if (format[i] == '%')
-			{	len = check_buffer_overflow(buffer, len);
-				buffer[len++] = format[i];
-				total_len++; }
+			{len = check_buffer_overflow(buffer, len);
+			buffer[len++] = format[i], total_len++; }
 			else
 			{	f = get_func(format[i]);
 				if (f == NULL)
-				{	len = check_buffer_overflow(buffer, len);
-					buffer[len++] = '%';
-					total_len++;
-					buffer[len++] = format[i];
-					total_len++; }
-				else
+				{	len = check_buffer_overflow(buffer, len), buffer[len++] = '%';
+					total_len++, buffer[len++] = format[i], total_len++; } else
 				{	str = f(list);
 					if (str == NULL)
-					{	va_end(list);
-						free(buffer);
+					{va_end(list), free(buffer);
 						return (-1); }
 					if (format[i] == 'c' && str[0] == '\0')
-					{	len = check_buffer_overflow(buffer, len);
-						buffer[len++] = '\0';
-						total_len++; }
-					for (j = 0; str[j]; j++)
-					{	len = check_buffer_overflow(buffer, len);
-						buffer[len++] = str[j];
-						total_len++; }
+					{len = check_buffer_overflow(buffer, len);
+					buffer[len++] = '\0', total_len++; }
+					for (j = 0; str[j]; j++, total_len++)
+					{len = check_buffer_overflow(buffer, len), buffer[len++] = str[j]; }
 					free(str);	}
-			}	i++;
-		}
+			}	i++;	}
 	}	write_buffer(buffer, len, list);
-	return (total_len);
-}
+	return (total_len); }
