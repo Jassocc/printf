af1773e1d23663348ddb517aff15d1c0c4aa4187
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-25 12:24:17 +0300

    some fixes

--- string.c
+++ string.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
 
 /**
  * print_s - takes as string and return
@@ -9,32 +8,22 @@
  */
 char *print_s(va_list str)
 {
-	char *s, *result;
-	int len, i, j;
+	char *s, *p;
+	int len;
 
 	s = va_arg(str, char *);
 	if (s == NULL)
-	{ s = "(NULL)"; }
-
-	len = _strlen(s);
-	result = malloc(sizeof(char) * (len * 4) + 1);
-
-	if (result == NULL)
-		return (NULL);
-
-	j = 0;
-	for (i = 0; i < len; i++)
 	{
-		if (s[i] >= 32 && s[i] < 127)
-		{
-			result[j++] = s[i];
-		} else
-		{
-			sprintf(result + j, "\\x%02X", (unsigned char)s[i]);
-			j += 4;
-		}
+		s = "(null)";
 	}
-	result[j] = '\0';
 
-	return (result);
+	len = strlen(s);
+
+	p = malloc(sizeof(char) * len + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	return (_strcpy(p, s));
 }
