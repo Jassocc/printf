03075d2eac66797b8fbd1e74d36d33034340629e
opiyoxbenji <opiyosbenji@gmail.com>
2023-07-25 11:20:07 +0300

    string

--- string.c
+++ string.c
@@ -1,26 +1,40 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * print_s - takes as string and return
  * @str:string
  * Return: string
  */
-
 char *print_s(va_list str)
 {
-	char *s, *p;
-	int len;
+	char *s, *result;
+	int len, i, j;
 
 	s = va_arg(str, char *);
 	if (s == NULL)
-	{
-		s = "(null)";
-	}
+	{ s = "(NULL)"; }
+
 	len = _strlen(s);
-	p = malloc(sizeof(char) * len + 1);
-	if (p == NULL)
-	{
+	result = malloc(sizeof(char) * (len * 4) + 1);
+
+	if (result == NULL)
 		return (NULL);
+
+	j = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] >= 32 && s[i] < 127)
+		{
+			result[j++] = s[i];
+		} else
+		{
+			sprintf(result + j, "\\x%02X", (unsigned char)s[i]);
+			j += 4;
+		}
 	}
-	return (_strcpy(p, s));
+	result[j] = '\0';
+
+	return (result);
 }
